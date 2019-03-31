9990730ae55a24cd7eeb45b54c57bcf677e7dc24
Nicholas Assaderaghi <nickassader@gmail.com>
2019-03-31 00:09:10 -0700

    Synchronizer class has been made. It checks whether or not it is connected with the phone.

--- Velotrack:AtheticsTracker/Velotrack:AtheticsTracker WatchKit Extension/Synchronizer.swift
+++ Velotrack:AtheticsTracker/Velotrack:AtheticsTracker WatchKit Extension/Synchronizer.swift
@@ -7,7 +7,36 @@
 //
 
 import WatchKit
+import WatchConnectivity
 
-class Synchronizer: NSObject {
+class Synchronizer: NSObject/*, WCSessionDelegate*/
+{
+    var canReachOtherDevice = false
 
+    func receiveData(data: [[Double]])
+    {
+        // receive and save that data
+    }
+    func checkIfCanConnect() -> Bool
+    {
+        if (canReachOtherDevice == true && WCSession.isSupported())
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
+    func sessionReachibilityDidChange(session: WCSession)
+    {
+        if session.isReachable
+        {
+            canReachOtherDevice = true
+        }
+        else
+        {
+            canReachOtherDevice = false
+        }
+    }
 }
